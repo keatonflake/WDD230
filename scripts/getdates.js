d7a309f98ca506fe853a3cae1020e641cded29ee
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2023-09-16 15:48:13 -0600

    added homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+let today1 = new Date();
+document.getElementById("currentyear").textContent="@" + today1.getFullYear();
+document.getElementById("lastmodified").innerHTML= "Last Modified: " + document.lastModified;
