d8a42ad655c729ece64f07a621be73f956956da8
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2023-11-03 22:23:29 -0600

    added home form

--- scripts/form.js
+++ scripts/form.js
@@ -1,22 +1,16 @@
-const form = document.getElementById('myForm');
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const confirmPasswordInput = document.getElementById('confirmPassword');
-const emailInput = document.getElementById('email');
-const ratingInput = document.getElementById('pageRating');
-const ratingDisplay = document.getElementById('ratingDisplay');
-const passwordError = document.getElementById('passwordError');
+const rating = document.getElementById("rating")
+rating.addEventListener('change', () => {
+    document.getElementById("currentrating").innerHTML = rating.value;
+})
 
-form.addEventListener('submit', function (event) {
-  if (passwordInput.value !== confirmPasswordInput.value) {
-    passwordError.textContent = 'Passwords do not match. Please try again.';
-    usernameInput.value = '';
-    passwordInput.value = '';
-    confirmPasswordInput.value = '';
-    event.preventDefault();
-  }
-});
-
-ratingInput.addEventListener('input', function () {
-  ratingDisplay.textContent = ratingInput.value;
-});
+const confirm = document.getElementById("confirm")
+confirm.addEventListener('blur', ()=>{
+    const password= document.getElementById("password")
+    if (password.value != confirm.value){
+        document.getElementById("message").innerHTML="Passwords do not match!"
+        password.focus()    
+    }
+    else{
+        document.getElementById("message").innerHTML=""
+    }
+})
