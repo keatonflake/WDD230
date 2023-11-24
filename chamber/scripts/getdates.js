45b9cb88d0d1e03ad1774677961b0f80b2a5a754
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2023-11-24 11:20:37 -0700

    added more images to demo lazy loading and fixed css errors

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,4 @@
+let today1 = new Date();
+document.getElementById("currentyear").textContent="@" + today1.getFullYear() + " Keaton Flake";
+document.getElementById("lastmodified").innerHTML= "Last Modified: " + document.lastModified;
+console.log("getdates");
